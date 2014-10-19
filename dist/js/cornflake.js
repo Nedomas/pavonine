752354868dc4fca7fa6ba638b1c9389a511047b6
Domas <domas.bitvinskas@me.com>
2014-10-20 01:19:57 +0300

    Initial prototype with create action working

--- dist/js/cornflake.js
+++ dist/js/cornflake.js
@@ -1,4 +1,86 @@
+var Cornflake, CornflakeSteps;
+
+Cornflake = (function() {
+  var act, bindStep, init, step_results;
+  step_results = {};
+  init = function() {
+    console.log('Here and now');
+    Godfather.API_URL = 'http://10.30.0.1:3000';
+    return CornflakeSteps.changeStep(1);
+  };
+  bindStep = function(model, attributes, action) {
+    return $(action.element).click(_.partial(act, model, attributes, action));
+  };
+  act = function(model, attributes, action, e) {
+    var attribute_values, connection;
+    e.preventDefault();
+    connection = new Godfather("" + model + "s");
+    attribute_values = _.inject(attributes, function(result, element, name) {
+      result[name] = element.val();
+      return result;
+    }, {});
+    return connection[action.name](attribute_values).then(function(record) {
+      var step_number;
+      step_number = 1;
+      step_results[step_number] = record;
+      return CornflakeSteps.changeStep(step_number + 1);
+    });
+  };
+  return {
+    init: init,
+    bindStep: bindStep
+  };
+})();
+
+CornflakeSteps = (function() {
+  var binding, changeStep, element, elements, hideAllBut, idx;
+  changeStep = function(i) {
+    hideAllBut(i);
+    return binding(i);
+  };
+  binding = function(i) {
+    var action, actionElement, attributeElements, attributes, model;
+    model = element(i).data('model');
+    attributeElements = element(i).find('*[data-attribute]');
+    attributes = _.inject(attributeElements, function(result, element) {
+      result[$(element).data('attribute')] = $(element);
+      return result;
+    }, {});
+    actionElement = element(i).find('*[data-action]').first();
+    action = {
+      element: actionElement,
+      name: $(actionElement).data('action')
+    };
+    return Cornflake.bindStep(model, attributes, action);
+  };
+  hideAllBut = function(dont_hide_i) {
+    return _.each(idx(), function(element, i) {
+      if (parseInt(i) === dont_hide_i) {
+        return element.show();
+      } else {
+        return element.hide();
+      }
+    });
+  };
+  element = function(i) {
+    return idx()[i];
+  };
+  idx = function() {
+    return _.inject(elements(), function(result, element) {
+      var step;
+      step = $(element).data('step') || 1;
+      result[step] = $(element);
+      return result;
+    }, {});
+  };
+  elements = function() {
+    return $('*[data-model], *[data-step]');
+  };
+  return {
+    changeStep: changeStep
+  };
+})();
+
 window.onload = function() {
-  Godfather.API_URL = 'http://10.30.0.1:3000';
-  return console.log('here and noew');
+  return Cornflake.init();
 };
--- src/coffee/cornflake.coffee
+++ src/coffee/cornflake.coffee
@@ -1,3 +1,76 @@
+Cornflake = (->
+  step_results = {}
+
+  init = ->
+    console.log('Here and now')
+    Godfather.API_URL = 'http://10.30.0.1:3000'
+    CornflakeSteps.changeStep(1)
+
+  bindStep = (model, attributes, action) ->
+    $(action.element).click(_.partial(act, model, attributes, action))
+
+  act = (model, attributes, action, e) ->
+    e.preventDefault()
+
+    connection = new Godfather("#{model}s")
+    attribute_values = _.inject(attributes, (result, element, name) ->
+      result[name] = element.val()
+      result
+    , {})
+
+    connection[action.name](attribute_values).then (record) ->
+      step_number = 1
+      step_results[step_number] = record
+      CornflakeSteps.changeStep(step_number + 1)
+
+  return {
+    init: init
+    bindStep: bindStep
+  }
+)()
+
+CornflakeSteps = (->
+  changeStep = (i) ->
+    hideAllBut(i)
+    binding(i)
+
+  binding = (i) ->
+    model = element(i).data('model')
+    attributeElements = element(i).find('*[data-attribute]')
+    attributes = _.inject(attributeElements, (result, element) ->
+      result[$(element).data('attribute')] = $(element)
+      result
+    , {})
+
+    actionElement = element(i).find('*[data-action]').first()
+    action = { element: actionElement, name: $(actionElement).data('action') }
+
+    Cornflake.bindStep(model, attributes, action)
+
+  hideAllBut = (dont_hide_i) ->
+    _.each idx(), (element, i) ->
+      if parseInt(i) == dont_hide_i
+        element.show()
+      else
+        element.hide()
+
+  element = (i) ->
+    idx()[i]
+
+  idx = ->
+    _.inject(elements(), (result, element) ->
+      step = $(element).data('step') or 1
+      result[step] = $(element)
+      result
+    , {})
+
+  elements = ->
+    $('*[data-model], *[data-step]')
+
+  return {
+    changeStep: changeStep
+  }
+)()
+
 window.onload = ->
-  Godfather.API_URL = 'http://10.30.0.1:3000'
-  console.log('here and noew')
+  Cornflake.init()
